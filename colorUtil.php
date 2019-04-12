d04c5887d1df65e5e5cd7594c1edb350f955433f
raul4247 <raulfmansur@hotmail.com>
2019-04-11 21:24:17 -0300

    rgb and rgba filtered

--- colorUtil.php
+++ colorUtil.php
@@ -9,10 +9,10 @@ function find_colors($content){
 
 function filter_colors($colors){
     // remove empty items
-    $colors = array_filter($colors); 
+    $colors = array_filter($colors);
 
     // Filtering color possibilities
-    
+
     foreach ($colors as &$color) {
         // HEX value, size 6
         if($color[0] == '#' && strlen($color) == 7)
@@ -21,13 +21,52 @@ function filter_colors($colors){
         if($color[0] == '#' && strlen($color) == 4)
             $color = strtoupper('#' . $color[1] . $color[1] . $color[2] . $color[2] . $color[3] . $color[3]);
 
-        //HSL values, RGBA values, and HSLA values
+		//RGB and RGBA values
+		if(substr($color, 0, 3) == 'rgb')
+			$color = RGB_to_HEX($color);
+
+        //HSL values
+
+		//HSLA values
+
     }
 
     // remove repeated items
     $colors = array_keys(array_flip($colors));
 
     return $colors;
+}
+
+function RGB_to_HEX($color) {
+	$rgba  = [];
+	$hex   = '';
+
+    if(preg_match_all('#\((([^()]+|(?R))*)\)#', $color ,$matches))
+    	$rgba = explode(',', implode(' ', $matches[1]));
+    else
+		$rgba = explode(',', $color);
+
+    $r = dechex($rgba['0']);
+    if(strlen($r) == 1)
+        $r = $r . $r;
+
+    $g = dechex($rgba['1']);  
+    if(strlen($g) == 1)
+        $g = $g . $g;
+
+    $b = dechex($rgba['2']);
+    if(strlen($b) == 1)
+        $b = $b . $b;
+
+    $a = '';
+
+	if(array_key_exists('3', $rgba))
+		$a = dechex($rgba['3'] * 255);
+
+    if(strlen($a) == 1)
+        $a = '0' . $a;
+
+	return strtoupper("#$a$r$g$b");
 }
 
 ?>
